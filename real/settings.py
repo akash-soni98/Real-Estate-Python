7ce73c9caa22702d7792665f8e1f08f99442baf4
akash-soni98 <103418697+akash-soni98@users.noreply.github.com>
2022-04-25 11:33:02 +0530

    files

--- real/settings.py
+++ real/settings.py
@@ -130,7 +130,6 @@ STATIC_ROOT= os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS =[
     os.path.join(BASE_DIR, 'real/static'),
-    os.path.join(BASE_DIR, 'static_cdn'),
 ]
 
 
